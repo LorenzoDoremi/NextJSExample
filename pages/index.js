2a67f2584198729e5ed4e922f81d9d927280ab76
Lorenzo <lorenzodoremi1994@gmail.com>
2022-09-01 19:08:26 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
+// come si possono importare componenti da altre pagine
+import handler from './api/hello'
+
+
 
 export default function Home() {
+
+  /* il più semplice hook */
+  const[counter, setCounter] = useState(1);
+
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +25,33 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Qua trovi un'altra<a href="second_page">Pagina!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {/* così si fa un commento */}
+         <CounterButton execution={setCounter} value={counter} />
+         <h2>{counter}</h2>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
-      </footer>
+       </main>
+     
     </div>
   )
+}
+
+// una funzione componente (come un bottone, un messaggio della chat, un'icona interattiva)
+function CounterButton({execution, value}) {
+
+  // una funzione che esegue la funzione ricevute (execution in questo. nome a scelta)
+  function handleCounter() {
+    console.log("press!")
+    execution(value*2)
+  }
+  return(
+
+    <button onClick={handleCounter}>Premi per raddoppiare il valore: {value}</button>
+  )
+
 }
---
+++ pages/second_page.js
@@ -0,0 +1,12 @@
+export default function second_page() {
+
+
+
+
+
+    return (
+
+
+        <div> <h1>CIAO! BENVENUTO!</h1></div>
+    )
+}
